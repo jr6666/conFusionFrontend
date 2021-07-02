af50acd3cf4c3cbb82cdd53d8dd74c925a550ae0
jr6666 <64059182+jr6666@users.noreply.github.com>
2021-07-02 22:54:54 +0530

    Update HomeComponent.js

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -14,7 +14,7 @@ function RenderCard({item,isLoading, errMess}){
             );
     return(
         <Card>
-            <CardImg src={baseUrl+item.image} alt={item.name} />
+            <CardImg src={item.image} alt={item.name} />
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 {item.designation ? 
