7c76aaf6e0ffcb47372d76d811cfd3ec55fdadde
jr6666 <64059182+jr6666@users.noreply.github.com>
2021-07-02 22:54:29 +0530

    Update DishDetailComponent.js

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -129,7 +129,7 @@ class CommentForm extends Component{
 function RenderDish({dish}){
     return(
         <Card key={dish.id}>
-            <CardImg width="100%" src={baseUrl+dish.image} alt={dish.name}/>
+            <CardImg width="100%" src={dish.image} alt={dish.name}/>
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
             </CardImgOverlay>
